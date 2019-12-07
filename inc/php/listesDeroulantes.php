63eacd4a1656caf35a7dfd424c088339bef20aaa
emincan <emin_can@live.fr>
2019-12-07 22:16:20 +0000

    Update listesDeroulantes.php

--- inc/php/listesDeroulantes.php
+++ inc/php/listesDeroulantes.php
@@ -1,20 +1,24 @@
 <div >
+  <i style="color:slategray;">Doit Contenir</i>
   <form id="formMustInclude">
     <input list="mustInclude" name="inputMust">
     <datalist id="mustInclude" >
     </datalist>
     <input type="submit" value="Ajouter" class="btn btn-secondary">
+    <input type="submit" id="viderMust" value="Vider" class="btn btn-secondary">
     <ul id="listeMustInclude">
     </ul>
   </form>
 </div>
 
 <div>
+  <i style="color:slategray;">Ne Doit Pas Contenir</i>
   <form id="formMustNotInclude">
     <input list="mustNotInclude" name="inputMustNot">
     <datalist id="mustNotInclude">
     </datalist>
     <input type="submit" value="Ajouter" class="btn btn-secondary">
+    <input type="submit" id="viderMustNot" value="Vider" class="btn btn-secondary">
     <ul id="listeMustNotInclude">
     </ul>
   </form>
