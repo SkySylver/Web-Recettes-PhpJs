ca44a8e53bafaa29d4fa34656d8d88637edbf6a8
Enes Usta <mine.skysylver@gmail.com>
2019-12-08 02:13:55 +0100

    ddd

--- panier.php
+++ panier.php
@@ -1,32 +1,28 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"].'/Projet/class/User.php';
-require_once $_SERVER["DOCUMENT_ROOT"].'/Projet/class/Panier.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/Projet/class/User.php';
+require_once $_SERVER["DOCUMENT_ROOT"] . '/Projet/class/Panier.php';
+
 use App\User;
 use App\Panier;
 use function App\isConnected;
+
 session_start();
 
-    if(!isset($_SESSION['panier'])) $_SESSION['panier'] = new Panier();
+if (!isset($_SESSION['panier'])) $_SESSION['panier'] = new Panier();
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-<?php $title='Panier'; require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/header.php'; ?>
+    <?php $title = 'Panier';
+    require $_SERVER["DOCUMENT_ROOT"] . '/Projet/inc/php/header.php'; ?>
 </head>
 
-<?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/nav.php'; ?>
+<?php require $_SERVER["DOCUMENT_ROOT"] . '/Projet/inc/php/nav.php'; ?>
 <body>
-
 <div id="panier">
-
-<?php
-$_SESSION['panier']->afficher(-1);
-?>
+    <?php $_SESSION['panier']->afficher(-1); ?>
 </div>
-
-<button onclick="AjouterPanier(0);">ffff</button>
 </body>
-
 </html>
