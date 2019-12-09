6d5060a8c11e3570d34399fd0a527da3022b134d
emincan <emin_can@live.fr>
2019-12-09 12:43:04 +0000

    Update home.php

--- home.php
+++ home.php
@@ -12,25 +12,28 @@
   
 <?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/nav.php'; ?>
 
-<div class="row">
-    <div id="Aliments" class="col-2">
-        <div>
-            <div id="Chemin" class="ml-4"><span class="btn-chemin">Hierarchie</span>/</div>
-            <ul id="Categories" class=""></ul>
-        <br>
-          <h4 style="color:darkslategray">Recherche par tag:</h4>
-          <?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/listesDeroulantes.php'; ?>
-          <button type="button" id="btn-rechercheParTag" class="btn btn-dark btn-rechercher">Rechercher</button>
-        </div>
-    </div>
-    <div class="col-10">
-      <div class="float-right">
-        <?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/barreDeRecherche.php'; ?>
-      </div>
-      <div class="ml-auto container">
-        <div id="Recettes" class="container "></div>
-      </div>
-    </div>
+<div class="container mx-auto">
+	<div class="row">
+		<div id="Aliments" class="col-sm-2">
+			<div>
+				<div id="Chemin" class="ml-4"><span class="btn-chemin">Hierarchie</span>/</div>
+				<div><ul id="Categories" class=""></ul></div>
+				<br>
+				<h4 style="color:darkslategray">Recherche par tag:</h4>
+				<?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/listesDeroulantes.php'; ?>
+				<button type="button" id="btn-rechercheParTag" class="btn btn-dark btn-rechercher">Rechercher</button>
+			</div>
+		</div>
+		<div class="col-sm-10">
+		  <div class="float-right">
+			<?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/barreDeRecherche.php'; ?>
+		  </div>
+		  <div >
+			<div id="Recettes" class="container "></div>
+		  </div>
+		</div>
+	</div>
 </div>
+
 </body>
 </html>
