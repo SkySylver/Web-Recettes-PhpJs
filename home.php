47263d33a2f8a4506b8bef11c90f136d13d176f1
emincan <emin_can@live.fr>
2019-12-09 14:16:46 +0000

    Update home.php

--- home.php
+++ home.php
@@ -9,12 +9,12 @@
     <script type="text/javascript" src="inc/js/home.js"></script>
 </head>
 <body>
-  
+
 <?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/nav.php'; ?>
 
 <div class="container mx-auto">
 	<div class="row">
-		<div id="Aliments" class="col-sm-2">
+		<div id="Aliments" class="col-sm-4">
 			<div>
 				<div id="Chemin" class="ml-4"><span class="btn-chemin">Hierarchie</span>/</div>
 				<div><ul id="Categories" class=""></ul></div>
@@ -24,12 +24,14 @@
 				<button type="button" id="btn-rechercheParTag" class="btn btn-dark btn-rechercher">Rechercher</button>
 			</div>
 		</div>
-		<div class="col-sm-10">
-		  <div class="float-right">
-			<?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/barreDeRecherche.php'; ?>
+		<div class="col-sm-8">
+      <br>
+      <div class="row-1 offset-8">
+			     <?php require $_SERVER["DOCUMENT_ROOT"].'/Projet/inc/php/barreDeRecherche.php'; ?>
 		  </div>
-		  <div >
-			<div id="Recettes" class="container "></div>
+      <br>
+		  <div class="row-11 ">
+			     <div id="Recettes" class="container "></div>
 		  </div>
 		</div>
 	</div>
